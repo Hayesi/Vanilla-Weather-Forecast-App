f7075c0628b75ad94fe746780687f8a347afef12
Hayesi <122321074+Hayesi@users.noreply.github.com>
2023-03-26 15:19:21 +0100

    Updated the function for forecast

--- src/app.js
+++ src/app.js
@@ -79,15 +79,19 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `
-  <div class="col-2">
-            <div class="card">
-              <div class="weather-forecast-date"></div>
-              <img src="" alt="" width="42" />
+      <div class="col-2">
+        <div class="card">
+          <div class="weather-forecast-date">${formatDay(forecastDay)}</div>
+            <img src="" alt="" width="42" />
               <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-temperature-max"></span>
-                <span class="weather-forecast-temperature-min"></span>
+                <span class="weather-forecast-temperature-max">${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="weather-forecast-temperature-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
               </div>
-            </div>
-          </div>
-          `;
+        </div>
+      </div>
+  `;
 }
